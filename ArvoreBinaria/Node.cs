9c2c1eaff2679e197e4168382bfca0aa68ab7638
Leandro Farias <leandro.sfarias997@gmail.com>
2021-09-28 00:12:41 -0300

    Árvore binária

---
+++ ArvoreBinaria/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArvoreBinaria
+{
+    class Node
+    {
+        public string Valor { get; set; }
+        public Node Esquerdo { get; set; }
+        public Node Direito { get; set; }
+        public Node Pai { get; set; }
+
+        // Construtor principal, recebe o conteúdo e o pai do nó
+        public Node(string valor, Node pai)
+        {
+            this.Valor = valor;
+            this.Pai = pai;
+            this.Direito = Esquerdo = null;
+        }
+
+        // Construtor para raiz
+        public Node(string valor) : this(valor, null) { }
+         
+        public Boolean TemFilhoEsquerdo()
+        {
+            if (Esquerdo == null)
+                return false;            
+            return true;
+        }
+
+        public Boolean TemFilhoDireito()
+        {
+            if (Direito == null)
+                return false;
+            return true;
+        }
+     
+        public override string ToString()
+        {
+            return this.Valor.ToString();
+        }
+    }
+}
---
+++ ArvoreBinaria/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArvoreBinaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+            arvoreBinaria.Insere(null, "A", 'E');
+            arvoreBinaria.Insere(arvoreBinaria.Raiz, "B", 'E');
+            Console.WriteLine(arvoreBinaria.Pega("B").Valor);
+        }
+    }
+}
